911c755ddf887caf7aa7784db879cac6371e5fb8
Tabs <tabata.prh@gmail.com>
2014-04-08 11:49:02 +0200

    Splits functions

--- src/webServer/WebServer.java
+++ src/webServer/WebServer.java
@@ -29,41 +29,7 @@ public class WebServer {
 			 * Infinitely listen to incoming connections
 			 * **/
 			for (;;) {
-				try {
-					// wait for a connection
-					Socket remote = s.accept();
-					// remote is now the connected socket
-					System.out.println("Connection, sending data.");
-					BufferedReader in = new BufferedReader(new InputStreamReader(
-							remote.getInputStream()));
-					PrintWriter out = new PrintWriter(remote.getOutputStream());
-
-					// read the data sent. We basically ignore it,
-					// stop reading once a blank line is hit. This
-					// blank line signals the end of the client HTTP
-					// headers.
-					String str = ".";
-					while (!str.equals(""))
-						str = in.readLine();
-
-					// Send the response
-					// Send the headers
-					out.println("HTTP/1.0 200 OK");
-					out.println("Content-Type: text/html");
-					out.println("Server: Bot");
-					// this blank line signals the end of the headers
-					out.println("");
-					// Send the HTML page
-					out.println("<H1>Micro-webserver</H1>");
-					out.println("<br></br>");
-					out.println("<br></br>");
-					out.println("<H2>Oh ja!</H2>");
-					out.flush();
-					remote.close();
-				} 
-				catch (Exception e) {
-					System.out.println("Could not accept remote request, due to  " + e.getMessage());
-				}
+				serverListens(s);
 			}
 		} 
 		catch (IOException io) {
@@ -77,8 +43,45 @@ public class WebServer {
 			s.close();
 		}
 
+}
+	private void serverListens(ServerSocket s){
+		try {
+			// wait for a connection
+			Socket remote = s.accept();
+			// remote is now the connected socket
+			System.out.println("Connection, sending data.");
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					remote.getInputStream()));
+			PrintWriter out = new PrintWriter(remote.getOutputStream());
 
+			// read the data sent. We basically ignore it,
+			// stop reading once a blank line is hit. This
+			// blank line signals the end of the client HTTP
+			// headers.
+			String str = ".";
+			while (!str.equals(""))
+				str = in.readLine();
+			returnOKResponse(out);
 
+			remote.close();
+		} 
+		catch (Exception e) {
+			System.out.println("Could not accept remote request, due to  " + e.getMessage());
+		}
+	}
+	
+	private void returnOKResponse(PrintWriter out){
+		out.println("HTTP/1.0 200");
+		out.println("Content-Type: text/html");
+		out.println("Server: Bot");
+		// this blank line signals the end of the headers
+		out.println("");
+		// Send the HTML page
+		out.println("<H1>Micro-webserver</H1>");
+		out.println("<br></br>");
+		out.println("<br></br>");
+		out.println("<H2>Oh ja!</H2>");
+		out.flush();
 	}
 
 	/**
